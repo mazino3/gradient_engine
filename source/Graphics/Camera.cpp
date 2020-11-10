6d149ecfcc6c6ae6c82db814077d39a1363ab2cb
Igor Ilyin <igor.ilin90@gmail.com>
2020-11-11 02:08:10 +0300

    Camera class implemented

---
+++ source/Graphics/Camera.cpp
@@ -0,0 +1,35 @@
+#include "Camera.h"
+#include <glm/ext.hpp>
+
+struct CameraImpl
+{
+	glm::mat4x4 projectionMatrix;
+	CameraType type;
+
+	CameraImpl() :
+		type(CameraType::None),
+		projectionMatrix(1.0f)
+	{}
+};
+
+Camera::Camera()
+{
+	_data = std::make_shared<CameraImpl>();
+}
+
+void Camera::setPerspective(float fov, float ratio, float nearPlane, float farPlane)
+{
+	_data->type = CameraType::Perspective;
+	_data->projectionMatrix = glm::perspective(fov, ratio, nearPlane, farPlane);
+}
+
+glm::mat4x4 Camera::getProjectionMatrix()
+{
+	return _data->projectionMatrix;
+}
+
+glm::mat4x4 Camera::getViewMatrix()
+{
+	return glm::lookAt(position, position + dirFront, dirUp);
+}
+
---
+++ source/Graphics/Camera.h
@@ -0,0 +1,35 @@
+#ifndef GRAPHICS_CAMERA_H
+#define GRAPHICS_CAMERA_H
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <memory>
+
+struct CameraImpl;
+
+enum class CameraType
+{
+	None,
+	Ortho,
+	Perspective
+};
+
+struct Camera
+{
+	glm::vec3 position;
+	glm::vec3 dirFront;
+	glm::vec3 dirUp;
+
+	Camera();
+
+	void setPerspective(float fov, float ratio, float nearPlane, float farPlane);
+	CameraType getCameraType();
+
+	glm::mat4x4 getProjectionMatrix();
+	glm::mat4x4 getViewMatrix();
+private:
+
+	std::shared_ptr<CameraImpl> _data;
+};
+
+#endif
