6ca383bb7a9b4dd14ff4333cfdb1de530fa97311
Igor Ilyin <igor.ilin90@gmail.com>
2020-11-23 19:55:20 +0300

    started working on lighting

---
+++ source/Graphics/Lights.h
@@ -0,0 +1,22 @@
+#ifndef GRAPHICS_LIGHTS_H
+#define GRAPHICS_LIGHTS_H
+
+#include <glm/vec3.hpp>
+
+struct PositionalLight
+{
+	glm::vec3 ambientColor;
+	glm::vec3 diffuseColor;
+	glm::vec3 spectacularColor;
+	glm::vec3 direction;
+};
+
+struct DirectionalLight
+{
+	glm::vec3 ambientColor;
+	glm::vec3 diffuseColor;
+	glm::vec3 spectacularColor;
+	glm::vec3 location;
+};
+
+#endif
---
+++ source/Graphics/Material.h
@@ -0,0 +1,9 @@
+#ifndef GRAPHICS_MATERIAL_H
+#define GRAPHICS_MATERIAL_H
+
+struct Material
+{
+
+};
+
+#endif
