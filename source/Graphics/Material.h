e96c12f155ef3f637371dfd4d045e87494338192
Igor Ilyin <igor.ilin90@gmail.com>
2020-11-24 23:06:11 +0300

    material properties

--- source/Graphics/Material.h
+++ source/Graphics/Material.h
@@ -1,8 +1,14 @@
 #ifndef GRAPHICS_MATERIAL_H
 #define GRAPHICS_MATERIAL_H
 
+#include <glm/vec3.hpp>
+
 struct Material
 {
+	glm::vec3 emission; //light produced by the material
+	glm::vec3 ambient; //what part of ambient light is reflected
+	glm::vec3 diffuse; //what part of diffuse light is scaterred
+	glm::vec3 spectacular; //what part of spectacular light is scattered
 	float shininess;
 };
 
