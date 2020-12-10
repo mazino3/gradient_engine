acfd2a8080ad2cdfb6919e0057d5712075be9d9c
Igor Ilyin <igor.ilin90@gmail.com>
2020-12-10 03:18:08 +0300

    added Vertex ctor only with position

--- source/Graphics/GeometryDefinition.cpp
+++ source/Graphics/GeometryDefinition.cpp
@@ -16,6 +16,15 @@ Vertex::Vertex() :
 	texCoords(0, 0)
 {}
 
+Vertex::Vertex(const vec3& pos) :
+	pos(pos),
+	normal(0, 0, 0),
+	tangent(0, 0, 0),
+	bitangent(0, 0, 0),
+	color(0, 0, 0, 0),
+	texCoords(0, 0)
+{}
+
 Vertex::Vertex(const vec3& pos, const vec3& normal, const vec4& color, const vec2& texCoords) :
 	pos(pos),
 	normal(normal),
--- source/Graphics/GeometryDefinition.h
+++ source/Graphics/GeometryDefinition.h
@@ -17,6 +17,7 @@ struct Vertex
 	glm::vec2 texCoords;
 
 	Vertex();
+	Vertex(const glm::vec3& pos);
 	Vertex(const glm::vec3& pos, const glm::vec3& normal, const glm::vec4& color, const glm::vec2& texCoords);
 	Vertex(const glm::vec3& pos, const glm::vec3& normal, const glm::vec3& tangent, const glm::vec3& bitangent, const glm::vec4& color, const glm::vec2& texCoords);
 };
