256c404afd7026835e315d2069ef1a61f58caa32
Igor Ilyin <igor.ilin90@gmail.com>
2021-03-14 21:49:52 +0300

    SimpleShader3d class added

---
+++ source/Graphics/SimpleShader3d.cpp
@@ -0,0 +1,31 @@
+#include "SimpleShader3d.h"
+
+SimpleShader3d::SimpleShader3d() : Shader(ShaderInitType::Filename, "Assets/Shaders/SimpleShader3d.vert", "Assets/Shaders/SimpleShader3d.frag")
+{
+}
+
+void SimpleShader3d::setModelMatrix(const glm::mat4x4& matrix)
+{
+	setUniform("model", matrix);
+}
+
+void SimpleShader3d::setProjectionMatrix(const glm::mat4x4& matrix)
+{
+	setUniform("projection", matrix);
+}
+
+void SimpleShader3d::setViewMatrix(const glm::mat4x4& matrix)
+{
+	setUniform("view", matrix);
+}
+
+void SimpleShader3d::setNormalMatrix(const glm::mat4x4& matrix)
+{
+	setUniform("normalTm", matrix);
+}
+
+void SimpleShader3d::setEyeDirection(const glm::vec3& eyeDirection)
+{
+	setUniform("eyeDirection", eyeDirection);
+}
+
---
+++ source/Graphics/SimpleShader3d.h
@@ -0,0 +1,19 @@
+#ifndef GRAPHICS_SIMPLE_SHADER_3D
+#define GRAPHICS_SIMPLE_SHADER_3D
+
+#include "Shader.h"
+
+struct SimpleShader3d : Shader
+{
+	SimpleShader3d();
+
+	void setProjectionMatrix(const glm::mat4x4& matrix);
+	void setViewMatrix(const glm::mat4x4& matrix);
+	void setModelMatrix(const glm::mat4x4& matrix);
+	void setNormalMatrix(const glm::mat4x4& matrix);
+
+	void setEyeDirection(const glm::vec3& eyeDirection);
+
+};
+
+#endif
