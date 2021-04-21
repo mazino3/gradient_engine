1ab4a4c6efc6ce9240229963eb2e22b3a9e5b63d
Igor Ilyin <igor.ilin90@gmail.com>
2021-04-21 22:56:30 +0300

    LineShader3d code

---
+++ source/Graphics/Graphics/Shaders/LineShader3d.cpp
@@ -0,0 +1,35 @@
+#include <Graphics/Shaders/LineShader3d.h>
+
+LineShader3d::LineShader3d() : Shader(ShaderInitType::Filename, "Assets/Shaders/LineShader3d.vert", "Assets/Shaders/LineShader3d.frag")
+{}
+
+void LineShader3d::setProjectionMatrix(const glm::mat4x4& matrix)
+{
+	setUniform("projection", matrix);
+}
+
+void LineShader3d::setViewMatrix(const glm::mat4x4& matrix)
+{
+	setUniform("view", matrix);
+}
+
+void LineShader3d::setPos1(const glm::vec3& pos)
+{
+	setUniform("pos1", pos);
+}
+
+void LineShader3d::setPos2(const glm::vec3& pos)
+{
+	setUniform("pos1", pos);
+}
+
+void LineShader3d::setEyeDirection(const glm::vec3& eyeDirection)
+{
+	setUniform("eyeDirection", eyeDirection);
+}
+
+void LineShader3d::setColor(const glm::vec3& color)
+{
+	setUniform("drawColor", color);
+}
+
---
+++ source/Graphics/Graphics/Shaders/LineShader3d.h
@@ -0,0 +1,20 @@
+#ifndef GRAPHICS_LINE_SHADER_3D
+#define GRAPHICS_LINE_SHADER_3D
+
+#include <Graphics/Shaders/Shader.h>
+
+struct LineShader3d : Shader
+{
+	LineShader3d();
+
+	void setProjectionMatrix(const glm::mat4x4& matrix);
+	void setViewMatrix(const glm::mat4x4& matrix);
+	
+	void setPos1(const glm::vec3& pos);
+	void setPos2(const glm::vec3& pos);
+	
+	void setEyeDirection(const glm::vec3& eyeDirection);
+	void setColor(const glm::vec3& color);
+};
+
+#endif
