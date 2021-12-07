fb1bc8a7e1f0f6dd2838e2cbae249ed400a90303
WIN-GU8IAO1M8LR\Игорь <ilin-igor96@yandex.ru>
2021-12-07 20:30:29 +0300

    created main.cpp for LevelEditor

---
+++ source/LevelEditor/LevelEditor/main.cpp
@@ -0,0 +1,65 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+
+#include <Graphics/RenderWindow.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <iostream>
+
+
+int main()
+{
+    const char* glsl_version = "#version 130";
+    RenderWindow window(800, 600, "Hello GLFW!");
+    if (!window.init())
+    {
+        std::cerr << "failed to create window";
+        return -1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window.getWindowPointer(), true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    window.setClearColor(glm::vec4(0.1f, 0.0f, 0.2f, 0.0f));
+
+
+    while (window.isOpen())
+    {
+        window.clear();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize;
+
+        ImGui::Begin("test window", nullptr, windowFlags);
+        ImGui::SetWindowSize(ImVec2(200.0f, 100.0f));
+        ImGui::SetWindowPos(ImVec2(0, 0));
+
+        ImGui::End();
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        window.swapBuffers();
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    return 0;
+}
