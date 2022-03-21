23369117b700cce2ef9bbc2cece3261944d9bdb2
WIN-GU8IAO1M8LR\Игорь <ilin-igor96@yandex.ru>
2022-03-22 01:33:29 +0300

    minor

--- source/GraphicsTest/GraphicsTest/main.cpp
+++ source/GraphicsTest/GraphicsTest/main.cpp
@@ -54,7 +54,7 @@ int main()
     {
         window.clear();
 
-        window.getInput().processInput();
+        window.getInput().processInput(); 
         if (currentScene != nullptr)
         {
             currentScene->render(window, 0.016f);
