d51f792f7b51434d57a54e07d196db08de9bae20
Igor Ilyin <igor.ilin90@gmail.com>
2022-05-10 22:30:29 +0300

    creating arrow AABBs inside of ResizeComponent

--- source/LevelEditor/LevelEditor/ResizeComponent.cpp
+++ source/LevelEditor/LevelEditor/ResizeComponent.cpp
@@ -1,10 +1,17 @@
 #include "ResizeComponent.h"
+#include "DependencyKeys.h"
+#include <functional>
+
+using namespace DependencyKeys;
 
 struct ResizeComponentImpl
 {
 	LevelObject& levelObject;
 	Renderer& renderer;
 	Resources& resources;
+	RaycastManager& arrowRaycastManager;
+
+	std::function<void(ArrowType)> arrowClickCallback;
 
 	std::weak_ptr<RenderObject> arrowMinusX;
 	std::weak_ptr<RenderObject> arrowPlusX;
@@ -13,25 +20,49 @@ struct ResizeComponentImpl
 	std::weak_ptr<RenderObject> arrowMinusZ;
 	std::weak_ptr<RenderObject> arrowPlusZ;
 
+	std::shared_ptr<AABB> aabbMinusX;
+	std::shared_ptr<AABB> aabbPlusX;
+	std::shared_ptr<AABB> aabbMinusY;
+	std::shared_ptr<AABB> aabbPlusY;
+	std::shared_ptr<AABB> aabbMinusZ;
+	std::shared_ptr<AABB> aabbPlusZ;
+
 	bool isSelected;
 
-	ResizeComponentImpl(LevelObject& levelObject, Renderer& renderer, Resources& resources) :
+	ResizeComponentImpl(LevelObject& levelObject, DepSupplier& depSupplier) :
 		levelObject(levelObject),
-		renderer(renderer),
-		resources(resources),
-		isSelected(false)
+		renderer(depSupplier.get<Renderer>()),
+		resources(depSupplier.get<Resources>()),
+		arrowRaycastManager(depSupplier.get(ARROW_RAYCAST_MANAGER)),
+		isSelected(false),
+		arrowClickCallback([](ArrowType arrowType) {})
 	{}
 
 	void updateArrowPositions();
+
+	std::function<void(void)> getRaycastCallback(ArrowType arrowType);
 };
 
-ResizeComponent::ResizeComponent(LevelObject& levelObject, Renderer& renderer, Resources& resources)
+std::function<void(void)> ResizeComponentImpl::getRaycastCallback(ArrowType arrowType)
 {
-	data = std::make_unique<ResizeComponentImpl>(levelObject, renderer, resources);
+	return [arrowType, this]() 
+	{
+		arrowClickCallback(arrowType);
+	};
+}
+
+ResizeComponent::ResizeComponent(LevelObject& levelObject, DepSupplier& depSupplier)
+{
+	data = std::make_unique<ResizeComponentImpl>(levelObject, depSupplier);
 }
 
 ResizeComponent::~ResizeComponent()
 {
+}
+
+void ResizeComponent::onArrowClicked(std::function<void(ArrowType)> callback)
+{
+	data->arrowClickCallback = callback;
 }
 
 void ResizeComponent::onSelected()
@@ -66,6 +97,20 @@ void ResizeComponent::onSelected()
 	data->arrowMinusZ = data->renderer.createRenderObject(*data->resources.getWhiteTexture().lock(), arrowDef.rotate(glm::vec3(180, 0, 0)), zArrowMat);
 	data->arrowPlusZ = data->renderer.createRenderObject(*data->resources.getWhiteTexture().lock(), arrowDef, zArrowMat);
 
+	data->aabbMinusX = std::make_shared<AABB>(glm::vec3(0, 0, 0), glm::vec3(0.2f, 0.2f, 0.2f));
+	data->aabbPlusX = std::make_shared<AABB>(glm::vec3(0, 0, 0), glm::vec3(0.2f, 0.2f, 0.2f));
+	data->aabbMinusY = std::make_shared<AABB>(glm::vec3(0, 0, 0), glm::vec3(0.2f, 0.2f, 0.2f));
+	data->aabbPlusY = std::make_shared<AABB>(glm::vec3(0, 0, 0), glm::vec3(0.2f, 0.2f, 0.2f));
+	data->aabbMinusZ = std::make_shared<AABB>(glm::vec3(0, 0, 0), glm::vec3(0.2f, 0.2f, 0.2f));
+	data->aabbPlusZ = std::make_shared<AABB>(glm::vec3(0, 0, 0), glm::vec3(0.2f, 0.2f, 0.2f));
+
+	data->arrowRaycastManager.registerAABB(*data->aabbMinusX, data->getRaycastCallback(ArrowType::MINUS_X));
+	data->arrowRaycastManager.registerAABB(*data->aabbPlusX, data->getRaycastCallback(ArrowType::PLUS_X));
+	data->arrowRaycastManager.registerAABB(*data->aabbMinusY, data->getRaycastCallback(ArrowType::MINUS_Y));
+	data->arrowRaycastManager.registerAABB(*data->aabbPlusY, data->getRaycastCallback(ArrowType::PLUS_Y));
+	data->arrowRaycastManager.registerAABB(*data->aabbMinusZ, data->getRaycastCallback(ArrowType::MINUS_Z));
+	data->arrowRaycastManager.registerAABB(*data->aabbPlusZ, data->getRaycastCallback(ArrowType::PLUS_Z));
+
 	data->arrowMinusX.lock()->renderLayer = 1;
 	data->arrowPlusX.lock()->renderLayer = 1;
 	data->arrowMinusY.lock()->renderLayer = 1;
@@ -86,6 +131,13 @@ void ResizeComponent::onUnselected()
 	data->renderer.removeRenderObject(*data->arrowPlusY.lock());
 	data->renderer.removeRenderObject(*data->arrowMinusZ.lock());
 	data->renderer.removeRenderObject(*data->arrowPlusZ.lock());
+
+	data->arrowRaycastManager.unregisterAABB(*data->aabbMinusX);
+	data->arrowRaycastManager.unregisterAABB(*data->aabbPlusX);
+	data->arrowRaycastManager.unregisterAABB(*data->aabbMinusY);
+	data->arrowRaycastManager.unregisterAABB(*data->aabbPlusY);
+	data->arrowRaycastManager.unregisterAABB(*data->aabbMinusZ);
+	data->arrowRaycastManager.unregisterAABB(*data->aabbPlusZ);
 }
 
 void ResizeComponentImpl::updateArrowPositions()
@@ -93,14 +145,26 @@ void ResizeComponentImpl::updateArrowPositions()
 	auto scale = levelObject.getScale();
 	auto pos = levelObject.getPosition();
 
-	arrowMinusX.lock()->transform.position = pos - glm::vec3(scale.x / 2 + 0.5f, 0, 0);
-	arrowPlusX.lock()->transform.position = pos + glm::vec3(scale.x / 2 + 0.5f, 0, 0);
+	auto posMinusX = pos - glm::vec3(scale.x / 2 + 0.5f, 0, 0);
+	auto posPlusX = pos + glm::vec3(scale.x / 2 + 0.5f, 0, 0);
+	auto posMinusY = pos - glm::vec3(0, scale.y / 2 + 0.5f, 0);
+	auto posPlusY = pos + glm::vec3(0, scale.y / 2 + 0.5f, 0);
+	auto posMinusZ = pos - glm::vec3(0, 0, scale.z / 2 + 0.5f);
+	auto posPlusZ = pos + glm::vec3(0, 0, scale.z / 2 + 0.5f);
 
-	arrowMinusY.lock()->transform.position = pos - glm::vec3(0, scale.y / 2 + 0.5f, 0);
-	arrowPlusY.lock()->transform.position = pos + glm::vec3(0, scale.y / 2 + 0.5f, 0);
+	arrowMinusX.lock()->transform.position = posMinusX;
+	arrowPlusX.lock()->transform.position = posPlusX;
+	arrowMinusY.lock()->transform.position = posMinusY;
+	arrowPlusY.lock()->transform.position = posPlusY;
+	arrowMinusZ.lock()->transform.position = posMinusZ;
+	arrowPlusZ.lock()->transform.position = posPlusZ;
 
-	arrowMinusZ.lock()->transform.position = pos - glm::vec3(0, 0, scale.z / 2 + 0.5f);
-	arrowPlusZ.lock()->transform.position = pos + glm::vec3(0, 0, scale.z / 2 + 0.5f);
+	aabbMinusX->position = posMinusX;
+	aabbPlusX->position = posPlusX;
+	aabbMinusY->position = posMinusY;
+	aabbPlusY->position = posPlusY;
+	aabbMinusZ->position = posMinusZ;
+	aabbPlusZ->position = posPlusZ;
 }
 
 void ResizeComponent::update()
--- source/LevelEditor/LevelEditor/ResizeComponent.h
+++ source/LevelEditor/LevelEditor/ResizeComponent.h
@@ -2,20 +2,35 @@
 #define RESIZE_COMPONENT_H
 
 #include <memory>
+#include <functional>
 #include "LevelObject.h"
 #include "Resources.h"
+#include "DependencyKeys.h"
+#include "DepSupplier.h"
+
+
+enum class ArrowType
+{
+	MINUS_X,
+	MINUS_Y,
+	MINUS_Z,
+	PLUS_X,
+	PLUS_Y,
+	PLUS_Z
+};
 
 struct ResizeComponentImpl;
 
 struct ResizeComponent
 {
-	ResizeComponent(LevelObject& levelObject, Renderer& renderer, Resources& resources);
+	ResizeComponent(LevelObject& levelObject, DepSupplier& depSupplier);
 	~ResizeComponent();
 
 	void onSelected();
 	void onUnselected();
-
 	void update();
+	void onArrowClicked(std::function<void(ArrowType)> callback);
+
 private:
 	std::unique_ptr<ResizeComponentImpl> data;
 };
